d5f8313130cbea176e3565d44a39cdc3ce0d8d47
Nelson Zheng <nelson.zheng@stonybrook.edu>
2021-10-29 01:59:13 -0400

    logout button functional

--- Top5Lists_HW4/client/src/auth/index.js
+++ Top5Lists_HW4/client/src/auth/index.js
@@ -7,7 +7,7 @@ console.log("create AuthContext: " + AuthContext);
 
 // THESE ARE ALL THE TYPES OF UPDATES TO OUR AUTH STATE THAT CAN BE PROCESSED
 export const AuthActionType = {
-    GET_LOGGED_IN: "GET_LOGGED_IN",
+    SET_LOGGED_IN: "SET_LOGGED_IN",
     REGISTER_USER: "REGISTER_USER"
 }
 
@@ -25,7 +25,7 @@ function AuthContextProvider(props) {
     const authReducer = (action) => {
         const { type, payload } = action;
         switch (type) {
-            case AuthActionType.GET_LOGGED_IN: {
+            case AuthActionType.SET_LOGGED_IN: {
                 return setAuth({
                     user: payload.user,
                     loggedIn: payload.loggedIn
@@ -50,6 +50,19 @@ function AuthContextProvider(props) {
                 payload: {
                     loggedIn: response.data.loggedIn,
                     user: response.data.user
+                }
+            });
+        }
+    }
+
+    auth.logoutUser = async function () {
+        const response = await api.getLoggedIn();
+        if (response.status === 200) {
+            authReducer({
+                type: AuthActionType.SET_LOGGED_IN,
+                payload: {
+                    loggedIn: false,
+                    user: null
                 }
             });
         }
